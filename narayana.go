5256e66e4fc57656557531bd5360a13c7dcada14
GerelNP92 <prof.laura.ue.loscastores@gmail.com>
2025-10-25 19:56:19 -0400

    Update and rename narayana.go to Pregunta5/narayana.go

--- narayana.go
+++
@@ -1,4 +0,0 @@
-package main
-import(."fmt";."math";."math/big")
-func c(n,k int)*Int{r:=NewInt(1);for i:=0;i<k;i++{r.Mul(r,NewInt(int64(n-i)));r.Div(r,NewInt(int64(i+1)))};return r}
-func main(){var n int;Scan(&n);k:=int(Log2(float64(n)));N:=c(n,k);N.Mul(N,c(n,k-1));N.Div(N,NewInt(int64(n)));a,b,d:=0,1,2;for i:=3;i<=int(Log2(float64(N.Int64())))+1;i++{a,b,d=b,d,a+b+d};Println(d)}
